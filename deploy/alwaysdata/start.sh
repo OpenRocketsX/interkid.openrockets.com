29c3ccc8c877f6f3c74fef4eef9eff2ba6970076
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-09-05 20:09:10 +0530

    Fix Granian startup on alwaysdata

--- deploy/alwaysdata/start.sh
+++ deploy/alwaysdata/start.sh
@@ -10,6 +10,5 @@ export GRANIAN_PORT="${PORT:-8300}"
 export GRANIAN_WORKERS="1"
 export GRANIAN_BLOCKING_THREADS="2"
 export GRANIAN_WEBSOCKETS="false"
-export GRANIAN_PROCESS_NAME="interkid-search"
 
 exec "$ROOT/venv/bin/granian" searx.webapp:app
