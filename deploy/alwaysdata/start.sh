0baa7b1552f55d854a24c77508a749c3784eea1b
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-09-06 07:11:51 +0530

    Run Interkid through verification route wrapper

--- deploy/alwaysdata/start.sh
+++ deploy/alwaysdata/start.sh
@@ -4,6 +4,7 @@ set -eu
 ROOT="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
 export SEARXNG_SETTINGS_PATH="$ROOT/config/settings.yml"
+export PYTHONPATH="$ROOT${PYTHONPATH:+:$PYTHONPATH}"
 export GRANIAN_INTERFACE="wsgi"
 export GRANIAN_HOST="${IP:-::}"
 export GRANIAN_PORT="${PORT:-8300}"
@@ -11,4 +12,4 @@ export GRANIAN_WORKERS="1"
 export GRANIAN_BLOCKING_THREADS="2"
 export GRANIAN_WEBSOCKETS="false"
 
-exec "$ROOT/venv/bin/granian" searx.webapp:app
+exec "$ROOT/venv/bin/granian" interkid_app:app
